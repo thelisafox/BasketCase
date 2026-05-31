8b21183025c8b4daa61a6021f905b9e7719e75de
CiprianCristian <40116411+CiprianCristian@users.noreply.github.com>
2026-05-31 09:56:55 +0200

    Update Interactable.cs

--- Assets/_Scripts/Interactions/Interactable.cs
+++ Assets/_Scripts/Interactions/Interactable.cs
@@ -3,28 +3,38 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    private int kaka = 0;
-
     private void OnMouseUpAsButton()
     {
         // call function needed according to object pressed
-        gameObject.SendMessage("Interact");
+        if (gameObject.name == "Button") gameObject.SendMessage("Interact");
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0) && kaka == 0)
+        if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
             if (Physics.Raycast(ray, out hit))
             {
-                if (hit.transform.name != "Button" && hit.transform.parent.GetComponent<Item>() != null && hit.transform.parent.name == name)
+                GameObject origin = null;
+                // if the interactable has itself the collider
+                if (hit.transform.gameObject == gameObject)
                 {
-                    kaka = 1;
-                    hit.transform.parent.SendMessage("Interact");
-                    print(hit.transform.parent.name);
+                    origin = hit.transform.gameObject;
+                    //hit.transform.SendMessage("Interact");
+                }
+                // if the child of the interactable has the collider
+                else if (hit.transform.parent.gameObject == gameObject)
+                {
+                    origin = hit.transform.parent.gameObject;
+                    //hit.transform.parent.SendMessage("Interact");
+                }
+
+                if (hit.transform.name != "Button" && origin != null)
+                {
+                    origin.SendMessage("Interact");
                 }
             }
         }
