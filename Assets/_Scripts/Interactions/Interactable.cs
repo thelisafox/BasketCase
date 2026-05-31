e6adca81a293594cf28917a4e3c68521d17fa82d
CiprianCristian <40116411+CiprianCristian@users.noreply.github.com>
2026-05-31 07:38:12 +0200

    something something

--- Assets/_Scripts/Interactions/Interactable.cs
+++ Assets/_Scripts/Interactions/Interactable.cs
@@ -1,10 +1,33 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    private void OnMouseUpAsButton()
+    private int kaka = 0;
+
+    /*private void OnMouseUpAsButton()
     {
         // call function needed according to object pressed
         gameObject.SendMessage("Interact");
+    }*/
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && kaka == 0)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name != "Button" && hit.transform.parent.GetComponent<Item>() != null)
+                {
+                    kaka = 1;
+                    hit.transform.parent.SendMessage("Interact");
+                    print(hit.transform.parent.name);
+                    print(hit.transform.name);
+                }
+            }
+        }
     }
 }
